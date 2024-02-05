64ca9002e26d179b8b790ec70d392dd5bb737fb6
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2024-02-05 12:03:56 +0100

    Add docker

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,7 +1,24 @@
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateReservationDto {
+    @IsDate()
+    @Type(() => Date)
     startDate: Date;
+
+    @IsDate()
+    @Type(() => Date)
     endDate: Date;
+
+    @IsString()
+    @IsNotEmpty()
     userId: string;
+
+    @IsString()
+    @IsNotEmpty()
     placeId: string;
+
+    @IsString()
+    @IsNotEmpty()
     invoiceId: string;
 }
